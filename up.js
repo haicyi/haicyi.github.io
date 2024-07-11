fef55ff0733c9c2154e411d957048c7629fa1f2f
haicyi <3450704430@qq.com>
2024-07-11 22:10:00 +0800

    Add files via upload

---
+++ up.js
@@ -0,0 +1,30 @@
+// 底端按钮
+window.onscroll=function(){
+    if(document.body.scrollTop>20||document.documentElement.scrollTop>20){
+        document.getElementById("myBtn").style.display="block";
+    }
+    else{
+        document.getElementById("myBtn").style.display="none";
+    }
+};
+
+function topFunction(){
+    document.body.scrollTop=0;
+    document.documentElement.scrollTop=0;
+}
+// ↑顶端按钮
+
+class Hanzi extends HTMLElement{
+    constructor(){
+        super();
+        const template=document.createElement('template');
+        template.innerHTML='<div>${this.textContent}</div>';
+        this.attachShadow({mode:'open'}).appendChild(template.content.cloneNode(true));
+    }
+}
+
+customElements.define('item',Hanzi);
+customElements.define('hanzi',Hanzi);
+customElements.define('word',Hanzi);
+customElements.define('phiou',Hanzi);
+customElements.define('zi',Hanzi)
